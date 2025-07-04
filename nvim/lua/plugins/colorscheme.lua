0841da650b74d5b9655c874b9cbc155f64d05d8d
Oliver <softwaredevdad@gmail.com>
2025-07-04 18:03:44 +0200

    Add Typescript LSP && Theme change

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,32 @@
+-- return {
+--   {
+--     "rockyzhang24/arctic.nvim",
+--     dependencies = { "rktjmp/lush.nvim" },
+--     name = "arctic",
+--     branch = "main",
+--     priority = 1000,
+--     config = function()
+--       vim.cmd("colorscheme arctic")
+--     end,
+--   },
+-- }
+
 return {
   {
-    "rockyzhang24/arctic.nvim",
-    dependencies = { "rktjmp/lush.nvim" },
-    name = "arctic",
-    branch = "main",
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
     priority = 1000,
+    -- you can set set configuration options here
     config = function()
-      vim.cmd("colorscheme arctic")
+      -- vim.g.zenbones_darken_comments = 45
+      vim.cmd.colorscheme("zenbones")
     end,
   },
 }
-
 -- return {
 --   {
 --     "gbprod/nord.nvim",
