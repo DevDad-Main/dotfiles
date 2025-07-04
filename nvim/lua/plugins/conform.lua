f09eca55f84618af4c455062dbe0e9b9c8b5623c
Oliver <softwaredevdad@gmail.com>
2025-07-04 22:52:43 +0200

    Allow yazi to use nvim instead of neovide

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,18 +6,19 @@ return {
       formatters_by_ft = {
         -- cs = { "csharpier" },
         html = { "prettier" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
       },
-      -- This will allow us to have prettier used with our EJS files
-      formatters = {
-        prettier = {
-          args = function(ctx)
-            if vim.endswith(ctx.filename, ".ejs") then
-              return { "--stdin-filepath", "$FILENAME", "--parser", "html" }
-            end
-            return { "--stdin-filepath", "$FILENAME" }
-          end,
-        },
-      },
+      -- -- This will allow us to have prettier used with our EJS files
+      -- formatters = {
+      --   prettier = {
+      --     args = function(ctx)
+      --       if vim.endswith(ctx.filename, ".ejs") then
+      --         return { "--stdin-filepath", "$FILENAME", "--parser", "html" }
+      --       end
+      --       return { "--stdin-filepath", "$FILENAME" }
+      --     end,
+      --   },
+      -- },
       -- Only required if we will make something in Unity
       -- formatters = {
       --   csharpier = {
