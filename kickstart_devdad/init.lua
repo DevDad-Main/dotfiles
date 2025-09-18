148eb1c2d92a6f7045d24ff67ffdd8e1be67f0b5
Oliver <softwaredevdad@gmail.com>
2025-09-18 16:09:07 +0200

    add custom lazyvim options into here. add auotpairs for auto completion of quotes, brackets etc. Main Syntax

--- kickstart_devdad/init.lua
+++ kickstart_devdad/init.lua
@@ -169,6 +169,42 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+vim.opt.number = true
+
+-- vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 0
+vim.opt.laststatus = 0
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.inccommand = 'split'
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = true
+vim.opt.backspace = { 'start', 'eol', 'indent' }
+vim.opt.path:append { '**' }
+vim.opt.wildignore:append { '*/node_modules/*' }
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitkeep = 'cursor'
+vim.opt.spelllang = 'en_gb'
+vim.o.hidden = true
+-- vim.opt.spell = true
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append { 'r' }
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
@@ -1018,7 +1054,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'javascript' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
---
+++ kickstart_devdad/lua/custom/plugins/nvim-autopairs.lua
@@ -0,0 +1,10 @@
+return {
+  -- In your plugins section
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = function()
+      require('nvim-autopairs').setup {}
+    end,
+  },
+}
