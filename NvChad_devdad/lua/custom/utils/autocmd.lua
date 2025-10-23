bfda66509573e2e8c5c662cadd08167d913d39b0
Oliver Metz <softwaredevdad@gmail.com>
2025-10-23 09:41:44 +0200

    refactor auto commands so we stop having so much lag when using a terminals within nvchad.

--- NvChad_devdad/lua/custom/utils/autocmd.lua
+++ NvChad_devdad/lua/custom/utils/autocmd.lua
@@ -438,8 +438,19 @@ autocmd("BufEnter", {
 -- Triger `autoread` when files changes on disk
 -- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
 -- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
+-- autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+--   command = [[silent! if mode() != 'c' && !bufexists("[Command Line]") | checktime | endif]],
+-- })
+
 autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
-  command = [[silent! if mode() != 'c' && !bufexists("[Command Line]") | checktime | endif]],
+  callback = function()
+    if vim.bo.buftype == "terminal" then
+      return
+    end
+    if vim.fn.mode() ~= "c" and not vim.fn.bufexists "[Command Line]" then
+      vim.cmd "checktime"
+    end
+  end,
 })
 
 -- Notification after file change
@@ -467,9 +478,19 @@ autocmd("BufDelete", {
   end,
 })
 
+-- autocmd({ "CursorHold", "CursorHoldI" }, {
+--   desc = "Run Lint",
+--   callback = function()
+--     require("lint").try_lint()
+--   end,
+-- })
+
 autocmd({ "CursorHold", "CursorHoldI" }, {
   desc = "Run Lint",
   callback = function()
+    if vim.bo.buftype == "terminal" then
+      return
+    end
     require("lint").try_lint()
   end,
 })
