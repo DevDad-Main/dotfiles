cac8c018ec28fab377fc6e66d37694aa5102fdb5
Oliver Metz <softwaredevdad@gmail.com>
2025-11-22 10:25:58 +0100

    refactor blink config to make it alot more usable, add more items from the suggestions menu and integrate it nicely with supermaven to not have conflicts. - set a custom hl so the bg blends nicely aswell and add my preferred keybinds of j/k as  idont like the default n/p

--- neovim/lua/setup/blink.lua
+++ neovim/lua/setup/blink.lua
@@ -1,6 +1,42 @@
+-- TODO: Move this to each individiual theme so we can have custom CmpNormal bgs for each theme.
+vim.api.nvim_set_hl(0, "CmpNormal", { bg = "#202029" })
+
+local function has_words_before()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  if col == 0 then
+    return false
+  end
+  local before = vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(1, col)
+  return not before:match("^%s*$")
+end
+
 return {
   keymap = {
-    preset = "enter",
+    -- preset = "enter",
+
+    ["<C-k>"] = { "select_prev", "fallback" },
+    ["<C-j>"] = { "select_next", "fallback" },
+    ["<Tab>"] = {
+      function()
+        if not has_words_before() then
+          return
+        end
+
+        local suggestion = require("supermaven-nvim.completion_preview")
+        if suggestion.has_suggestion() then
+          vim.schedule(function()
+            suggestion.on_accept_suggestion()
+          end)
+        end
+        return true
+      end,
+    },
+    ["<S-Tab>"] = { "snippet_backward", "fallback" },
+    ["<Esc>"] = {
+      "cancel",
+      "fallback",
+    },
+    ["Enter"] = { "select_and_accept", "fallback" },
   },
   appearance = {
     use_nvim_cmp_as_default = true,
@@ -18,11 +54,19 @@ return {
     },
   },
   completion = {
-    documentation = { window = { border = "single" } },
-    menu = {
-      border = "single",
+    documentation = {
+      window = {
+        border = "rounded",
+        winblend = 15,
+      },
     },
-    -- ghost_text = { enabled = true },
+    menu = {
+      draw = { treesitter = { "lsp" } },
+      border = "rounded",
+      winblend = 5,
+      winhighlight = "Normal:CmpNormal",
+    },
+    ghost_text = { enabled = true },
     list = {
       selection = {
         preselect = function(ctx)
@@ -34,5 +78,15 @@ return {
       },
     },
   },
-  signature = { window = { border = "single" } },
+  documentation = {
+    auto_show = true,
+    auto_show_delay_ms = 500,
+  },
+  signature = {
+    enabled = true,
+    window = {
+      border = "rounded",
+      winblend = 15,
+    },
+  },
 }
