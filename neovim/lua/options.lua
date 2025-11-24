064fa8abf32864416bfae44551e34359815a2170
Oliver Metz <softwaredevdad@gmail.com>
2025-11-24 11:22:43 +0100

    update diagnostics with nicer cleaner icons. (bit bigger also but not too big). - Also virtual lines, gives a nice indiciation when we are on the line with that issue.

--- neovim/lua/options.lua
+++ neovim/lua/options.lua
@@ -151,15 +151,22 @@ vim.diagnostic.config({
   underline = true,
   update_in_insert = false,
   severity_sort = false,
-  -- virtual_lines = {
-  --   current_line = true,
-  -- },
+
+  --NOTE: Comment this out to disable inline virtual diagnostics
+  virtual_lines = {
+    current_line = true,
+  },
+
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = "󰅚 ",
-      [vim.diagnostic.severity.WARN] = "󰳦 ",
-      [vim.diagnostic.severity.HINT] = "󱡄 ",
-      [vim.diagnostic.severity.INFO] = " ",
+      [vim.diagnostic.severity.ERROR] = "󰅙 ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = "󰌵",
+      [vim.diagnostic.severity.INFO] = "󰋼 ",
+      -- [vim.diagnostic.severity.ERROR] = "󰅚 ",
+      -- [vim.diagnostic.severity.WARN] = "󰳦 ",
+      -- [vim.diagnostic.severity.HINT] = "󱡄 ",
+      -- [vim.diagnostic.severity.INFO] = " ",
     },
   },
 })
