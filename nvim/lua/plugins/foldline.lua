3771aca53681a0d42005a4cdd3c86ddfddce466a
Oliver Metz <softwaredevdad@gmail.com>
2026-05-12 16:08:27 +0200

    feat: Catppuccin Theme Variant, Foldline ui modifications.

--- nvim/lua/plugins/foldline.lua
+++ nvim/lua/plugins/foldline.lua
@@ -7,11 +7,12 @@ return {
     vim.g.fold_line_char_top_close = "+" -- default: fillchars.foldclose or "+"
     vim.g.fold_line_char_close = "├" -- default: fillchars.vertright or "├"
     vim.g.fold_line_char_open_sep = "│" -- default: fillchars.foldsep or "│"
-    vim.g.fold_line_char_open_start = "╭" -- default: "┌"
-    vim.g.fold_line_char_open_end = "╰" -- default: "└"
+    -- vim.g.fold_line_char_open_start = "╭" -- default: "┌"
+    -- vim.g.fold_line_char_open_end = "└" -- default: "└"
     vim.g.fold_line_char_open_start_close = "╒" -- default: "╒"
     vim.g.fold_line_char_open_end_close = "╘" -- default: "╘"
 
-    vim.g.fold_line_current_fold_only = true
+    -- Shows only the current line.
+    vim.g.fold_line_current_fold_only = false
   end,
 }
