a308a488be8558103272bf80952f95f6872b06e1
Oliver Metz <softwaredevdad@gmail.com>
2026-07-09 11:56:43 +0200

    feat: Theme update, font change and tilde removal from column line

--- .doom.d/appearance.el
+++ .doom.d/appearance.el
@@ -5,17 +5,26 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; (setq doom-theme 'doom-one)
-(setq doom-theme 'doom-gruvbox)
+;; (setq doom-theme 'doom-gruvbox)
 ;; (setq doom-theme 'catppuccin)
 ;; (setq catppuccin-flavor 'mocha)
 
-(setq doom-font (font-spec :family "IoskeleyMono Nerd Font" :size 15 :weight 'medium)
-      doom-variable-pitch-font (font-spec :family "IoskeleyMono Nerd Font" :size 15))
+(add-to-list 'custom-theme-load-path "~/.config/dotfiles/.doom.d/")
 
-(set-frame-parameter (selected-frame) 'alpha '(85 85))
-(add-to-list 'default-frame-alist '(alpha 85 85))
+(load-theme 'nezburn t)
+;; (setq doom-font (font-spec :family "IoskeleyMono Nerd Font" :size 15 :weight 'medium)
+;;       doom-variable-pitch-font (font-spec :family "IoskeleyMono Nerd Font" :size 15))
+
+(setq doom-font (font-spec :family "Iosevka Nerd Font" :size 17 :weight 'medium)
+      doom-variable-pitch-font (font-spec :family "Iosevka Nerd Font" :size 17))
+
+(set-frame-parameter (selected-frame) 'alpha '(95 95))
+(add-to-list 'default-frame-alist '(alpha 95 95))
 
 (setq display-line-numbers-type 'relative)
+
+(after! vi-tilde-fringe
+  (global-vi-tilde-fringe-mode -1))
 
 (setq org-directory "~/org/")
 (setq select-enable-clipboard t)
---
+++ .doom.d/custom.el
@@ -0,0 +1,15 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e8bd9bbf6506afca133125b0be48b1f033b1c8647c628652ab7a2fe065c10ef0"
+     "3613617b9953c22fe46ef2b593a2e5bc79ef3cc88770602e7e569bbd71de113b" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
