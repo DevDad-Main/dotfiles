4ad9a05813a3d4fa5b80a337489c67813c65ed60
Oliver Metz <softwaredevdad@gmail.com>
2026-07-12 11:45:37 +0200

    fix: correct font family to Iosevka Nerd Font

--- i3/keyhelp.sh
+++ i3/keyhelp.sh
@@ -40,5 +40,5 @@ Volume keys       ·  Volume up/down/mute
 Brightness keys   ·  Brightness up/down
 Media keys        ·  Play/Pause/Next/Prev
 EOF
-kitty --class keyhelp -o font_size=13 sh -c "fzf --no-info --bind=esc:abort,enter:abort < '$tmp'"
+kitty --class keyhelp -o font_size=12 sh -c "fzf --no-info --bind=esc:abort,enter:abort < '$tmp'"
 rm -f "$tmp"
