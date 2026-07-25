a8e799e2674b9b08b665805f65be9eaebdfcdee3
Oliver Metz <softwaredevdad@gmail.com>
2026-07-25 14:22:46 +0200

    fix: kill existing redshift processes before toggling night light

--- i3/scripts/night-light.sh
+++ i3/scripts/night-light.sh
@@ -3,13 +3,15 @@
 TEMP=${NIGHT_TEMP:-4500}
 LATLON="54.352:18.646"
 flag=/tmp/redshift-enabled
+
+pkill -9 redshift 2>/dev/null
+
 if [ -f "$flag" ]; then
     rm -f "$flag"
-    notify-send "Night Light" "Disabled"
     nohup redshift -l "$LATLON" -t 6500:"$TEMP" >/dev/null 2>&1 &
+    notify-send "Night Light" "Disabled"
 else
     touch "$flag"
-    pkill -9 redshift 2>/dev/null
     redshift -P -O "$TEMP"
     notify-send "Night Light" "Enabled (${TEMP}K)"
 fi
